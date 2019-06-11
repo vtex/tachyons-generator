dcc252a02c66b4a49f998d2183b9de1255484c3f
Christian Kaisermann <christian@kaisermann.me>
2019-06-11 16:09:20 -0300

    Add tests for print stylesheet

---
+++ test/print-test.js
@@ -0,0 +1,20 @@
+import test from 'ava'
+import tachyonsGenerator from '../'
+
+test('should generate a print stylesheet', async t => {
+  const tachy = tachyonsGenerator()
+  const printStylesheet = await tachy.generatePrint({ key: 'print' })
+  t.snapshot(printStylesheet)
+})
+
+test('should generate a print stylesheet with custom key identifier', async t => {
+  const tachy = tachyonsGenerator()
+  const printStylesheet = await tachy.generatePrint({ key: 'p' })
+  t.snapshot(printStylesheet)
+})
+
+test('should generate a minified print stylesheet', async t => {
+  const tachy = tachyonsGenerator()
+  const printStylesheet = await tachy.generatePrint({ key: 'p', minify: true })
+  t.snapshot(printStylesheet)
+})
