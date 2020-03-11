10017843869a1e2f7f87d3e61521c4d987f40188
Joao Fidelis <joaogfidelis@gmail.com>
2020-03-11 10:45:32 -0300

    separate

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 'use strict'
 
-const camelcase = require('camelcase')
-const buildCss = require('./lib/build.js')
-const DEFAULT_CONFIG = require('./config')
+const { parse: mqifyParse } = require('mqify')
+const stripComments = require('strip-css-comments')
+const classPostfix = require('postcss-class-postfix')
+const postcss = require('postcss')
 
+const DEFAULT_CONFIG = require('./config')
 const generateDocs = require('./docs')
+
+const buildCss = require('./lib/build.js')
+const generators = require('./lib/generators')
 const generate = require('./lib/generate')
 const generatePrint = require('./lib/generatePrint')
 const assembleCss = require('./lib/assemble-css')
@@ -54,6 +59,42 @@ module.exports = config => {
     return docs
   }
 
-  function generator () {}
+  generator.generateSeparateStylesheets = async (options) => {
+    const modules = await generate(_config, mediaQueries, true)
+
+    const keysToAddClasses = Object.keys(modules).filter(key => generators.hasOwnProperty(key))
+    const parsedMediaQueries = mqifyParse(mediaQueries)
+    const modulesWithTokens = await Promise.all(mediaQueries.map(mq => {
+      const token = Object.keys(mq)[0]
+      if (!token) {
+        return null
+      }
+      const moduleWithToken = keysToAddClasses.reduce(
+        (acc, curKey) => {
+          acc[curKey] = postcss([classPostfix('-' + token)]).process(modules[curKey]).css
+          return acc
+        },
+        {}
+      )
+      moduleWithToken.normalize = ''
+      return { module: moduleWithToken, mq: parsedMediaQueries.find(mqP => mqP.key === token).mq }
+    }))
+
+    const allModules = [{ module: modules }, ...modulesWithTokens.filter(Boolean)]
+
+    const allCss = await Promise.all(allModules.map(moduleObj => {
+      const post = assembleCss(moduleObj.module, _config)
+      return buildCss(post, options).then(({ css }) => {
+        if (!moduleObj.mq) {
+          return css
+        }
+
+        return `@media ${moduleObj.mq} {\n${stripComments(css).trim()}\n}`
+      })
+    }))
+    return allCss
+  }
+
+  function generator() { }
   return generator
 }
